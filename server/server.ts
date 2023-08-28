4809977954783af5fb28dd3bdf1930b9846ec1cc
FelipeColona <felipedev202@gmail.com>
2023-08-27 23:42:58 -0300

    feat: add enter room functionality

--- server/server.ts
+++ server/server.ts
@@ -3,14 +3,15 @@ import { v4 as uuidv4 } from 'uuid'
 
 interface Data{
     event: string
+    payload: string | number
 }
 
 class Room{
-    roomId: string
+    id: string
     sockets: string[] = []
     
     constructor(roomId: string){
-      this.roomId = roomId
+      this.id = roomId
     }
 }
 
@@ -20,6 +21,7 @@ declare class ExtWebSocketServer extends WebSocketServer {
 
 declare interface ExtWebSocket extends WebSocket {
     id: string
+    roomId: string
 }
 
 //@ts-ignore
@@ -32,7 +34,7 @@ server.on('connection', (ws: ExtWebSocket) => {
 
   ws.on('message', (dataStr) => {
     //@ts-ignore
-    const data: Data = JSON.parse(dataStr)
+    let data: Data = JSON.parse(dataStr)
 
     if (data.event == "createRoom") {
       //Create room
@@ -41,6 +43,7 @@ server.on('connection', (ws: ExtWebSocket) => {
       
       //Add socket to room
       room.sockets.push(ws.id)
+      ws.roomId = roomId
       
       //Add room to rooms array
       server.rooms.push(room)
@@ -51,6 +54,17 @@ server.on('connection', (ws: ExtWebSocket) => {
       
       //Send room id back to client
       ws.send(`${roomId}`)
+    }
+    else if(data.event == "enterRoom"){
+      server.rooms.forEach( (room) => {
+        if(room.id == data.payload){
+          //Add socket to room
+          room.sockets.push(ws.id)
+          //Add roomId to socket
+          ws.roomId = room.id
+          console.log(`New client connected: ${ws.id}`)
+        }
+      })
     }
     else if(data.event == "pause"){
       console.log("PAUSE")
